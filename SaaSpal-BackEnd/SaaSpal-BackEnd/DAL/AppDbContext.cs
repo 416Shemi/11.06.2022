bb0dd0c3872bc8c3f042cfc122181fbbc665eb72
416Shemi <tu7ngcsvr@code.edu.az>
2022-06-11 11:13:02 +0400

    new project

---
+++ SaaSpal-BackEnd/SaaSpal-BackEnd/DAL/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SaaSpal_BackEnd.DAL
+{
+    public class AppDbContext:DbContext
+    {
+
+    }
+}
